4a5064df1242a97c0948189c5bd3bf4831125331
Hpalling <105853474+Hpalling@users.noreply.github.com>
2022-06-01 11:42:38 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace PersonalityTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //prog küsib kasutajalt siseestada tema lemmikvärv;
+            //kui kasutajalt sisetab 'punane', konsool kuvad 'oled romantiline'
+            //kui kasutaja sisestab 'sinine', konsool kuvad 'oled töökas'
+            //kui kasutaja sisestab 'rohelin', konsool kuva 'oled looduseinimne'
+            //kui kasutaja sisestab midagi muud, konsool kuvad 'oled {userColor} ükssarvik';
+
+            Console.WriteLine("Sisestada oma lemmikvärv");
+            string userColor = Console.ReadLine();
+
+            if(userColor == "punane")
+            {
+                Console.WriteLine("Oled Romantiline.") ;
+            }
+            else if (userColor == "sinine")
+            {
+                Console.WriteLine("Oled Töökas");
+            }
+            else if (userColor == "roheline")
+            {
+                Console.WriteLine("oled looduse sõber");
+            }
+            else
+            {
+                Console.WriteLine($"oled{userColor} Ükssarvik");
+            }
+        }
+    }
+}
